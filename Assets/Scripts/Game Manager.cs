b2c6b97d5b9343c78c9824d01a91d3ebf356a804
TavaresFr <152510118+TavaresFr@users.noreply.github.com>
2024-05-25 20:04:46 +0100

    Lesson 5.3 - Game Over

--- Assets/Scripts/Game Manager.cs
+++ Assets/Scripts/Game Manager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> targets;
@@ -10,10 +11,16 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] TextMeshProUGUI scoreText;
 
+    [SerializeField] TextMeshProUGUI gameOverText;
+
+    [HideInInspector] public bool isGameActive;
+
     int score = 0;
 
     void Start()
     {
+        isGameActive = true;
+        gameOverText.gameObject.SetActive(false);
         StartCoroutine(SpawnTarget());
         score = 0;
         UpdateScore(0);
@@ -21,12 +28,23 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while (true)
+        while (isGameActive)
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
         }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
     }
 
     public void UpdateScore(int scoreToAdd)
